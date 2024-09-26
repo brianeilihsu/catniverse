f1cfc79696561942bdb8eb36a3487734921b2e79
LAPTOP-HPCOM7NP\User <enya20021209@gmail.com>
2024-09-26 23:52:18 +0800

    map modify

--- frontend/catniverse-frontend/src/Components/Map/Map.jsx
+++ frontend/catniverse-frontend/src/Components/Map/Map.jsx
@@ -1,12 +1,85 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "../Header/Header";
+import { simplemaps_countrymap_mapdata } from "./mapData";
+import TaiwanMapSVG from "../../Image/tw.svg";
+import './Map.css';
 
 function Map() {
-  const [count, setCount] = useState(0);
+  const [tooltipContent, setTooltipContent] = useState("");
+
+  // 處理每個區域的動態效果
+  const handleMouseOver = (regionId) => {
+    const regionData = simplemaps_countrymap_mapdata.state_specific[regionId];
+    if (regionData) {
+      setTooltipContent(`${regionData.name}`);
+    }
+  };
+
+  const handleMouseOut = () => {
+    setTooltipContent("");
+  };
+
+  // 將地圖區域的樣式動態綁定
+  const getRegionStyle = (regionId) => {
+    const regionData = simplemaps_countrymap_mapdata.state_specific[regionId];
+    if (regionData) {
+      return {
+        fill: regionData.color,
+        cursor: "pointer",
+      };
+    }
+    return {};
+  };
 
   return (
     <div>
       <Header />
+      <div className="map-container">
+        <h2>台灣互動地圖</h2>
+        <div style={{ position: "relative" }}>
+          <img
+            src={TaiwanMapSVG}
+            alt="Taiwan Map"
+            style={{ width: "100%", height: "auto" }}
+            useMap="#taiwan-map"
+          />
+          {/* 添加互動區域 */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 800 600"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            {/* 假設 Changhua 的 path 是以下這段 */}
+            <path
+              id="TWCHA"
+              d="M563.1 480.3l4.2 1.3 2.9 2.5 1.9 4.6 0.7 5.5 3.3 3.9 8.5 2.5 3.2 2.6 2.3 3.6 1.4 2.9 0 2.8 0.9 2.5 3.1 0.5 3.9 2.6 0.1 1 0 2.6-3.1 1.1-0.9 2.1 0.4 2.9-1.4 3.3-1.2 3.9-0.1 4.1-0.8 4.7-0.7 9.7 1.5 4.5 2.6 2.4 2.8 1 4.1 2.1-0.7 2.5-2.8 1.1-1.8 1.7-1.7 1.1-0.7 0.5-8.7-1.7-4.2 0.1-8.7-4.4-14.2-1.6-8.4-3.2-4.8-1.3-5.4-0.3-15.9 1.9-5.5-3.2 0.6-1 1.1-5.1 0.9-2.1 5.1-4.9 1.5-2.1 1.9-5.6 3-5.6 4.2-11.6 1.9-3.3 2.2-2.2 1.8-1.2 1.5-0.8 1.2-1 0.4-2.2 0.9-2 3.7-2.9 0.8-2.2 0.4-7.2 0.7-2 1.9-1.9 8.2-9.5z" // 使用真實的 path
+              style={getRegionStyle("TWCHA")}
+              onMouseOver={() => handleMouseOver("TWCHA")}
+              onMouseOut={handleMouseOut}
+            />
+            
+            {/* 其他區域... */}
+          </svg>
+        </div>
+        {tooltipContent && (
+          <div
+            style={{
+              position: "absolute",
+              top: "10px",
+              left: "10px",
+              background: "white",
+            }}
+          >
+            {tooltipContent}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ frontend/catniverse-frontend/src/Components/Map/mapData.js
@@ -0,0 +1,319 @@
+// mapData.js
+export const simplemaps_countrymap_mapdata = {
+    main_settings: {
+      width: "responsive",
+      background_color: "#FFFFFF",
+      background_transparent: "yes",
+      border_color: "#ffffff",
+      state_description: "State description",
+      state_color: "#88A4BC",
+      state_hover_color: "#3B729F",
+      state_url: "",
+      border_size: 1.5,
+      all_states_inactive: "no",
+      all_states_zoomable: "yes",
+      location_description: "Location description",
+      location_url: "",
+      location_color: "#FF0067",
+      location_opacity: 0.8,
+      location_hover_opacity: 1,
+      location_size: 25,
+      location_type: "square",
+      location_image_source: "frog.png",
+      location_border_color: "#FFFFFF",
+      location_border: 2,
+      location_hover_border: 2.5,
+      all_locations_inactive: "no",
+      all_locations_hidden: "no",
+      label_color: "#ffffff",
+      label_hover_color: "#ffffff",
+      label_size: 16,
+      label_font: "Arial",
+      label_display: "auto",
+      label_scale: "yes",
+      hide_labels: "no",
+      hide_eastern_labels: "no",
+      zoom: "yes",
+      manual_zoom: "yes",
+      back_image: "no",
+      initial_back: "no",
+      initial_zoom: "-1",
+      initial_zoom_solo: "no",
+      region_opacity: 1,
+      region_hover_opacity: 0.6,
+      zoom_out_incrementally: "yes",
+      zoom_percentage: 0.99,
+      zoom_time: 0.5,
+      popup_color: "white",
+      popup_opacity: 0.9,
+      popup_shadow: 1,
+      popup_corners: 5,
+      popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+      popup_nocss: "no",
+      div: "map",
+      auto_load: "yes",
+      url_new_tab: "no",
+      images_directory: "default",
+      fade_time: 0.1,
+      link_text: "View Website",
+      popups: "detect",
+    },
+    state_specific: {
+      TWCHA: {
+        name: "Changhua",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default",
+      },
+      TWCYI: {
+        name: "Chiayi City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWCYQ: {
+        name: "Chiayi",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWHSQ: {
+        name: "Hsinchu",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWHSZ: {
+        name: "Hsinchu City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWHUA: {
+        name: "Hualien",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWILA: {
+        name: "Yilan",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWKEE: {
+        name: "Keelung City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWKHH: {
+        name: "Kaohsiung City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWKIN: {
+        name: "Kinmen",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWLIE: {
+        name: "Matsu Islands",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWMIA: {
+        name: "Miaoli",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWNAN: {
+        name: "Nantou",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWNWT: {
+        name: "New Taipei City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWPEN: {
+        name: "Penghu",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWPIF: {
+        name: "Pingtung",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWTAO: {
+        name: "Taoyuan",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWTNN: {
+        name: "Tainan City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWTPE: {
+        name: "Taipei City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWTTT: {
+        name: "Taitung",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWTXG: {
+        name: "Taichung City",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      },
+      TWYUN: {
+        name: "Yunlin",
+        description: "default",
+        color: "default",
+        hover_color: "default",
+        url: "default"
+      }
+    },
+    locations: {
+      "0": {
+        name: "Taipei",
+        lat: "25.047763",
+        lng: "121.531846",
+      },
+    },
+    labels: {
+      TWCHA: {
+        name: "Changhua",
+        parent_id: "TWCHA",
+      },
+      TWCYI: {
+        name: "Chiayi City",
+        parent_id: "TWCYI"
+      },
+      TWCYQ: {
+        name: "Chiayi",
+        parent_id: "TWCYQ"
+      },
+      TWHSQ: {
+        name: "Hsinchu",
+        parent_id: "TWHSQ"
+      },
+      TWHSZ: {
+        name: "Hsinchu City",
+        parent_id: "TWHSZ"
+      },
+      TWHUA: {
+        name: "Hualien",
+        parent_id: "TWHUA"
+      },
+      TWILA: {
+        name: "Yilan",
+        parent_id: "TWILA"
+      },
+      TWKEE: {
+        name: "Keelung City",
+        parent_id: "TWKEE"
+      },
+      TWKHH: {
+        name: "Kaohsiung City",
+        parent_id: "TWKHH"
+      },
+      TWKIN: {
+        name: "Kinmen",
+        parent_id: "TWKIN"
+      },
+      TWLIE: {
+        name: "Matsu Islands",
+        parent_id: "TWLIE"
+      },
+      TWMIA: {
+        name: "Miaoli",
+        parent_id: "TWMIA"
+      },
+      TWNAN: {
+        name: "Nantou",
+        parent_id: "TWNAN"
+      },
+      TWNWT: {
+        name: "New Taipei City",
+        parent_id: "TWNWT"
+      },
+      TWPEN: {
+        name: "Penghu",
+        parent_id: "TWPEN"
+      },
+      TWPIF: {
+        name: "Pingtung",
+        parent_id: "TWPIF"
+      },
+      TWTAO: {
+        name: "Taoyuan",
+        parent_id: "TWTAO"
+      },
+      TWTNN: {
+        name: "Tainan City",
+        parent_id: "TWTNN"
+      },
+      TWTPE: {
+        name: "Taipei City",
+        parent_id: "TWTPE"
+      },
+      TWTTT: {
+        name: "Taitung",
+        parent_id: "TWTTT"
+      },
+      TWTXG: {
+        name: "Taichung City",
+        parent_id: "TWTXG"
+      },
+      TWYUN: {
+        name: "Yunlin",
+        parent_id: "TWYUN"
+      }
+    },
+    legend: {
+      entries: [],
+    },
+    regions: {},
+  };
+  
