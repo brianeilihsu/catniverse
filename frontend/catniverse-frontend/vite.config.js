f3390bc0725cffcbcea5e2f31be8a7d00f04a41c
LAPTOP-HPCOM7NP\User <enya20021209@gmail.com>
2024-11-14 11:01:27 +0800

    ok

--- frontend/catniverse-frontend/vite.config.js
+++ frontend/catniverse-frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000000, 
+    outDir: 'build',
+  },
+  
+  server: {
+    host: '172.20.10.4',  
+    port: 5173,          
+  }
 })
